4711705c3211bda1645737c5abeba5666e6f3238
Britannio <britanniojarrett@gmail.com>
2024-03-29 13:25:03 +0100

    finish chapter 4 & 5

--- NOTES.md
+++ NOTES.md
@@ -3,3 +3,24 @@
 
 `x` is an expression. Adding a semi colon turns it into a void statement.
 
+# Chapter 4 Ownership
+
+Rust will 'move' variables rather than making shallow copies of them.
+
+```rust
+let s1 = String::from("hello");
+let s2 = s1;
+
+println!("{}, world!", s1);
+```
+
+`s1` stores a heap allocated `String`. It (the heap pointer) is then moved to 
+`s2` which invalidates `s1` so this is no longer valid.
+
+If `s1` were a string literal though, the heap wouldn't be involved and it would
+work. This works for any type that implements the `Copy` trait.
+
+---
+
+Types can implement a `drop` function when they are freed to free their memory.
+
--- chapter3/functions/src/main.rs
+++ chapter3/functions/src/main.rs
@@ -4,6 +4,15 @@ fn main() {
     another_function();
     let f4 = fibonacci(4);
     println!("fib 4 = {f4}");
+    
+
+    let s = String::from("this is the string s");
+    takes_ownership(&s);
+    
+    // we can't use it again because takes_ownership destroys it...
+    println!("{s}");
+    // Unless takes_ownership accepts &String i.e. a string reference.
+    println!("{}", dangle2());
 }
 
 fn another_function() {
@@ -18,4 +27,14 @@ fn fibonacci(n: i32) -> i32 {
     } else {
         fibonacci(n - 1) + fibonacci(n - 2)
     }
+}
+
+
+fn takes_ownership(strin: &String) {
+    println!("{strin}");
+}
+
+fn dangle2() -> String {
+    let s = String::from("hello");
+    s
 }
